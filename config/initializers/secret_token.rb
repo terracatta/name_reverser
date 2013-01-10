8457fb9d9a117c39f567afeb2c79bf1a04eb389d
Jason Meller <jason.meller@gmail.com>
2013-01-10 14:44:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NameReverser::Application.config.secret_token = '212718cec4d9abc2608078a71bdc97d9cd2cf296aae00c0d5babe7d7e06bb1298ca6c72f25f7550b715d4b2cbf5fcfa9ff16520386ddef0ceec4652e31c78165'
